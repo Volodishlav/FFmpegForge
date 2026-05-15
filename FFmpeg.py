02f3a65fafa5ffce8cb1e4d0232b52dde6a8059d
JZ <132818012+Vladispy@users.noreply.github.com>
2026-05-15 14:10:18 +0200

    update FFmpeg.py

--- FFmpeg.py
+++ FFmpeg.py
@@ -21,20 +21,33 @@ def main():
 
     customwidth = int(input("Enter width (Default: 1920): ") or 1920)
     compression = int(input("Enter compression level (1-31 | Lower = better quality | Default: 4): ") or 4)
-    
+
     RUN(customwidth, compression)
 
 def setCommand(file: str, customwidth: int, compression: int, OutputFile: str) -> list[str]:
-    command = [
-        "ffmpeg", "-y", "-i", str(file),
-        # resize if exceeds 1920px in width
-        # "-vf", "scale='min(3840,iw)':-2",
-        "-vf", f"scale={customwidth}:-2",
-        # quality / compression level
-        "-q:v", str(compression),
-    ]
 
-    command.append(str(OutputFile))
+    if file.suffix.lower() == ".gif":
+        vf = (
+            f"fps=10,scale={customwidth}:-2:flags=lanczos,"
+            "split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse"
+        )
+
+        command = [
+            "ffmpeg", "-y", "-i", str(file),
+            "-vf", vf,
+            str(OutputFile)
+        ]
+
+    else:
+        command = [
+            "ffmpeg", "-y", "-i", str(file),
+            # resize if exceeds 1920px in width
+            # "-vf", "scale='min(3840,iw)':-2",
+            "-vf", f"scale={customwidth}:-2",
+            # quality / compression level
+            "-q:v", str(compression),
+            str(OutputFile)
+        ]
 
     return command
 
@@ -46,7 +59,7 @@ def RUN(customwidth, compression):
         OutputFile = Path(OUTPUT_DIR) / f"{file.stem}{file.suffix.lower()}"
         command = setCommand(file, customwidth, compression, OutputFile)
 
-        print(f"Compressing: {file.name} {command}")
+        print(f"Compressing: {file.name}")
 
         subprocess.run(
             command,
